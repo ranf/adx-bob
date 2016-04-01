a368a2d71aab0edffc2a6de4bf1e30b24d40d30b
nofarGez <sochern@gmail.com>
2016-04-01 10:57:08 +0300

    Adding bid bundle files

---
+++ adx-bob/src/tau/tac/adx/agents/bob/bid/BidBundleData.java
@@ -0,0 +1,80 @@
+package tau.tac.adx.agents.bob.bid;
+
+import java.util.Random;
+
+public class BidBundleData
+{
+  double avgPerImp;
+  double daysLeftFactor;
+  double campaignImpRatio;
+  double randomFactor;
+  double gameDayFactor;
+  
+  public void set_avgPerImp(double budget, long reachImps)
+  {
+    this.avgPerImp = (budget / reachImps);
+  }
+  
+  public double get_avgPerImp()
+  {
+    return this.avgPerImp;
+  }
+  
+  public void set_daysLeftFactor(int daysLeft, int totalCampaignDays)
+  {
+    if (daysLeft == 1) {
+      this.daysLeftFactor = 2.7D;
+    }
+    if (daysLeft == 2) {
+      this.daysLeftFactor = 1.8D;
+    } else {
+      this.daysLeftFactor = (1.2D * (1 - (totalCampaignDays - daysLeft) / 10));
+    }
+  }
+  
+  public double get_daysLeftFactor()
+  {
+    return this.daysLeftFactor;
+  }
+  
+  public void set_campaignImpRatio(int ImpLeft, int TotalImp, int daysLeft, int totalCampaignDays)
+  {
+    this.campaignImpRatio = ((ImpLeft / TotalImp) / (daysLeft / totalCampaignDays));
+  }
+  
+  public double get_campaignImpRatio()
+  {
+    return this.campaignImpRatio;
+  }
+  
+  public void set_randomFactor()
+  {
+	  double days_left;
+	  double camp_ratio;
+	  days_left = get_daysLeftFactor();
+	  camp_ratio =  get_campaignImpRatio();
+	  
+	  if ((days_left <= 3) && (camp_ratio > 0.55))
+	  {
+		  this.randomFactor = randDouble(0.95, 1);
+	  }
+	  
+	  else if (randDouble(0, 1) < 0.2)
+	  {
+		  this.randomFactor = Math.max(marketSegmentPopularity / 2, randDouble(0,1));
+	  }	  
+  }
+  
+  public double get_randomFactor()
+  {
+    return this.randomFactor;
+  }
+  
+  public static double randDouble(double min, double max) 
+  {
+	    double random = new Random().nextDouble();
+	    double result = min + (random * (max - min));
+
+	    return result;
+  }
+}
---
+++ adx-bob/src/tau/tac/adx/agents/bob/bid/BidBundleSrtategy.java
@@ -0,0 +1,21 @@
+package tau.tac.adx.agents.bob.bid;
+
+import tau.tac.adx.agents.CampaignData;
+import tau.tac.adx.props.AdxBidBundle;
+
+public class BidBundleSrtategy {
+	
+	  AdxBidBundle Current_bid_bundle_parm;
+	  CampaignData[] running_Campaigns;
+	  
+	  public double calc_stable_bid(BidBundleData parambidBundleData)
+	  {
+	    throw new Error("Unresolved compilation problems: \n\tC cannot be resolved to a variable\n\tSyntax error on token \"*\", ; expected\n");
+	  }
+	  
+	  public double calc_first_days_bid(double paramDouble1, double paramDouble2, double paramDouble3)
+	  {
+	    throw new Error("Unresolved compilation problem: \n");
+	  }
+
+}
