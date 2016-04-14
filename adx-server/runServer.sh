46762a41b053c7bc580d1e15e292f728dac5fbdd
Ran Feder <ran.feder@outlook.com>
2016-04-14 23:46:35 +0300

    added giza and agent00 agents

--- adx-server/runServer.sh
+++ adx-server/runServer.sh
@@ -8,4 +8,6 @@ TACAA_HOME=`pwd`
 echo $TACAA_HOME
 echo $CLASSPATH
 
+java -cp "lib/*" tau.tac.adx.agentware.Main -config config/giza-aw-1.conf &
+java -cp "lib/*" tau.tac.adx.agentware.Main -config config/agent00-aw-1.conf &
 java -cp "lib/*" se.sics.tasim.sim.Main
