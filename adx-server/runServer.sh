93437b9d29ec57e0f897a046282d6c95465255eb
Ran Feder <ran.feder@outlook.com>
2016-04-15 00:20:31 +0300

    added bcm and oos agents. game results look completely random.
    closes #13

--- adx-server/runServer.sh
+++ adx-server/runServer.sh
@@ -10,4 +10,6 @@ echo $CLASSPATH
 
 java -cp "lib/*" tau.tac.adx.agentware.Main -config config/giza-aw-1.conf &
 java -cp "lib/*" tau.tac.adx.agentware.Main -config config/agent00-aw-1.conf &
+java -cp "lib/*" tau.tac.adx.agentware.Main -config config/bcm-aw-1.conf &
+java -cp "lib/*" tau.tac.adx.agentware.Main -config config/oos-aw-1.conf &
 java -cp "lib/*" se.sics.tasim.sim.Main
