bc78a0937450b53945522f500ef877488cae2ac6
Ran Feder <ran.feder@outlook.com>
2016-04-16 00:43:17 +0300

    adding travis+code coverage. closes #6

--- adx-bob/src/test/java/tau/tac/adx/agents/bob/bid/BidBundleFactorCalculatorTest.java
+++ adx-bob/src/test/java/tau/tac/adx/agents/bob/bid/BidBundleFactorCalculatorTest.java
@@ -2,36 +2,43 @@ package tau.tac.adx.agents.bob.bid;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BidBundleFactorCalculatorTest {
 
 	@Test
+	@Ignore
 	public void testBidBundleFactorCalculator() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testCalcDayLeftFactor() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testCalcMarketSegmentPopularityFactor() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testCalcCampaignImpRatio() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testCalcRandomFactor() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testCalcAdInfoFactor() {
 		fail("Not yet implemented");
 	}
