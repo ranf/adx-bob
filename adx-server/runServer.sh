05ed35d30baf9396b1fd28bcc7fa433a59c194a2
Ran Feder <ran.feder@outlook.com>
2016-04-19 15:00:10 +0300

    added ibm agent

--- adx-server/runServer.sh
+++ adx-server/runServer.sh
@@ -12,4 +12,5 @@ java -cp "lib/*" tau.tac.adx.agentware.Main -config config/giza-aw-1.conf &
 java -cp "lib/*" tau.tac.adx.agentware.Main -config config/agent00-aw-1.conf &
 java -cp "lib/*" tau.tac.adx.agentware.Main -config config/bcm-aw-1.conf &
 java -cp "lib/*" tau.tac.adx.agentware.Main -config config/oos-aw-1.conf &
+java -cp "lib/*" tau.tac.adx.agentware.Main -config config/ibm-aw-1.conf &
 java -cp "lib/*" se.sics.tasim.sim.Main
