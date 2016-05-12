ba7eea78c2a58c9891cfa99d071bc049241c8d33
Ran Feder <ran.feder@outlook.com>
2016-05-12 18:43:27 +0300

    fixed another agent configuration

--- adx-server/winRunServer.cmd
+++ adx-server/winRunServer.cmd
@@ -3,7 +3,9 @@ start java -cp "lib/*" tau.tac.adx.agentware.Main -config config/giza-aw-1.conf
 start java -cp "lib/*" tau.tac.adx.agentware.Main -config config/agent00-aw-1.conf
 start java -cp "lib/*" tau.tac.adx.agentware.Main -config config/bcm-aw-1.conf
 ::start java -cp "lib/*" tau.tac.adx.agentware.Main -config config/oos-aw-1.conf
-start java -cp "lib/*" tau.tac.adx.agentware.Main -config config/x-aw-1.conf
+::start java -cp "lib/*" tau.tac.adx.agentware.Main -config config/x-aw-1.conf
 start java -cp "lib/*" tau.tac.adx.agentware.Main -config config/ibm-aw-1.conf
 cd lib2\wiz
 start java -cp "lib/*;." tau.tac.adx.agentware.Main -config config/aw-1.conf
+cd ..\x
+start java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-1.conf
